f8bd8c819e832dee5ab0e908d2c7f8b522920238
Ted Schreiber <tedschreiber@teds-mbp.home>
2021-09-30 17:09:34 -0400

    attempting to fix unexpected < char

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <HashRouter>
     <App />
     </HashRouter>,
-  // </React.StrictMode>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
